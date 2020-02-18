78c5c9af84f5e469eb5f7a063d919e15fc9d3666
asharalam11 <44848905+asharalam11@users.noreply.github.com>
2020-02-17 23:55:00 -0800

    Added chmod a+x

--- setup_jetson.sh
+++ setup_jetson.sh
@@ -8,6 +8,10 @@ mv gripper_publisher.py ~/catkin_ws/src/asl_turtlebot/scripts/
 
 mv camera_transform_relay.py ~/catkin_ws/src/asl_turtlebot/scripts/
 
+# Make the python files executable
+chmod a+x ~/catkin_ws/src/asl_turtlebot/scripts/gripper_publisher.py
+chmod a+x ~/catkin_ws/src/asl_turtlebot/scripts/camera_transform_relay.py
+
 # Copy the new launch file to the appropriate folder
 
 mv turtlebot3_bringup_jetson_pi_cs237b.launch ~/catkin_ws/src/asl_turtlebot/launch/
