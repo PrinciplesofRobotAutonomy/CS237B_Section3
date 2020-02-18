c3bf80fc322590d837ef7ec1430c794fc6e14268
asharalam11 <44848905+asharalam11@users.noreply.github.com>
2020-02-18 12:35:44 -0800

    Typo

--- setup_pi.sh
+++ setup_pi.sh
@@ -13,6 +13,6 @@ sudo make install
 
 # Enable pigpiod as a startup service
 tar xf pigiod.service.d.tar
-sudo cp -r pigpio.service.d /etc/systemd/system
+sudo cp -r pigpiod.service.d /etc/systemd/system
 sudo systemctl enable pigpiod
 sudo systemctl start pigpiod
