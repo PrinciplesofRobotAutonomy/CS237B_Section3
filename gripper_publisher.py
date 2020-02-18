77daf5e6d30f437a4d97c0775b92aefe9e38845c
asharalam11 <44848905+asharalam11@users.noreply.github.com>
2020-02-17 23:02:22 -0800

    Removed check for rospy_is_shutdown()

--- gripper_publisher.py
+++ gripper_publisher.py
@@ -7,15 +7,20 @@ def gripper_publisher(pwm):
 	pub = rospy.Publisher('gripper_pose', Int8, queue_size = 10)
 	rospy.init_node('gripper_publisher', anonymous = True)
 	rate = rospy.Rate(10)
-	while not rospy.is_shutdown():
-		pub.publish(pwm)
-		rate.sleep()
+	#while not rospy.is_shutdown():
+	pub.publish(pwm)
+	rate.sleep()
 
 if __name__ == '__main__':
 	try:
 		while(1):
-			key = getKey()
-			gripper_publisher(key)
+			key = int(input("Enter 1 to close and 0 to open the gripper : "))
+			if key == 1:
+				gripper_publisher(0)
+			elif key == 0:
+				gripper_publisher(100)
+			else:
+				break
 			print("")
 	except:
 		pass
