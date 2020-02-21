92a84535fe6e4895928f85075c4b87f8a6f2d0a6
jennalee <yunjung.jenna.lee@gmail.com>
2020-02-20 16:08:16 -0800

    chmod for all

--- setup_jetson.sh
+++ setup_jetson.sh
@@ -2,17 +2,17 @@
 
 # Copy the gripper_publisher script to the appropriate folder
 
-mv gripper_publisher.py ~/catkin_ws/src/asl_turtlebot/scripts/
+mv gripper_publisher.py ~/catkin_ws/src/<your repo name>/scripts/
 
 # Copy the camera_transform_relay to the appropriate folder
 
-mv camera_transform_relay.py ~/catkin_ws/src/asl_turtlebot/scripts/
+mv camera_transform_relay.py ~/catkin_ws/src/<your repo name>/scripts/
 
 # Make the python files executable
-chmod a+x ~/catkin_ws/src/asl_turtlebot/scripts/gripper_publisher.py
-chmod a+x ~/catkin_ws/src/asl_turtlebot/scripts/camera_transform_relay.py
+chmod a+x ~/catkin_ws/src/<your repo name>/scripts/gripper_publisher.py
+chmod a+x ~/catkin_ws/src/<your repo name>/scripts/camera_transform_relay.py
 
 # Copy the new launch file to the appropriate folder
 
-mv turtlebot3_bringup_jetson_pi_cs237b.launch ~/catkin_ws/src/asl_turtlebot/launch/
+mv turtlebot3_bringup_jetson_pi_cs237b.launch ~/catkin_ws/src/<your repo name>/launch/
 
